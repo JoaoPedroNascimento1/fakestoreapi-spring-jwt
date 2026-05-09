2883f131e4a55e18f454b268a80bd4b878bd40a0
Joao Pedro Nascimento <nascjp.dev@gmail.com>
2026-05-09 15:55:17 -0300

    Primeiro commit

---
+++ src/test/java/br/com/fakestoreapi/FakestoreapiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fakestoreapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FakestoreapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
